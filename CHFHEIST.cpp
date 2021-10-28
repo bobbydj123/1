645a48d9448e328ec7e4d3f4d6f24c621ff02be9
Aman Kumar Gupta <aman18217@iiitd.ac.in>
2021-10-28 11:06:37 +0530

    updated cpp

---
+++ CHFHEIST.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define filewr() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+
+void solve(){
+	int total, interval, base, inc;
+	cin >> total >> interval >> base >> inc;
+	
+	
+
+}
+
+int main(){
+	fastio();
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
